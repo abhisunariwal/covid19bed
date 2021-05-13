687677c4ad91157030d9ecfc02e1f0a5e4adfe32
abhisunariwal <abhisunariwal@gmail.com>
2021-05-13 13:40:34 +0530

    adding script for heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const axios = require('axios');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // set up template engine
@@ -53,4 +55,4 @@ app.use((req, res) => {
     res.render('404page.ejs');
 });
 
-app.listen(3000);
+app.listen(port);
